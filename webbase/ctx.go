f2bbade74c60d2ef7f82015525cd507049c5dfbb
Billy Chen <shixuan163556@163.com>
2022-03-06 12:16:02 +0800

    add mysql and mongo libs

--- webbase/ctx.go
+++ webbase/ctx.go
@@ -1,6 +1,15 @@
 package webbase
 
-import "github.com/gin-gonic/gin"
+import (
+	"time"
+
+	"github.com/gin-contrib/zap"
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
+	"github.com/virgoC0der/go-base/logging"
+	"github.com/virgoC0der/go-base/valid"
+)
 
 type CommonResp struct {
 	Code    int         `json:"code"`
@@ -26,4 +35,19 @@ func ServeResponse(c *gin.Context, errMsg *CommonResp, args ...interface{}) {
 		Data:    data,
 	}
 	c.JSON(200, result)
+}
+
+// InitServer inits gin server
+func InitServer() *gin.Engine {
+	logging.InitLog()
+
+	g := gin.New()
+	g.Use(ginzap.Ginzap(logging.Logger, time.RFC3339, true))
+	g.Use(ginzap.RecoveryWithZap(logging.Logger, true))
+
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		valid.RegisterValidator(v)
+	}
+
+	return g
 }
